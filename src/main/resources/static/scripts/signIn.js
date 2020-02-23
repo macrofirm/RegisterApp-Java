bbd60b1979c25a7072637a2f118369ce05d4c33e
Calvin Franz <cmfranz@uark.edu>
2020-02-22 19:14:17 -0600

    Add validate sign-in.

--- src/main/resources/static/scripts/signIn.js
+++ src/main/resources/static/scripts/signIn.js
@@ -1,8 +1,29 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-	// TODO: Anything you want to do when the page is loaded?
+document.addEventListener("DOMContentLoaded", () => {
 });
 
-function validateForm() {
-	// TODO: Validate the user input
-	return true;
+function getEmployeeIdElement() {
+	return document.getElementById("employeeID");
+}
+
+function getEmployeePasswordElement() {
+	return document.getElementById("password");
+}
+
+function getSignInActionElement() {
+	return document.getElementById("signInButton");
+}
+
+function validateSignIn() {
+	employeeID = getEmployeeIdElement();
+	password = getEmployeePasswordElement();
+	invalid = false;
+	if(Number.isInteger(employeeID) || employeeID.value == "") {
+		displayError("Invalid ID.");
+		invalid = true;
+	}
+	else if(password.value == "") {
+		displayError("Invalid Password.")
+		invalid = true;
+	}
+	return invalid;
 }
