94f5ff61c518e83fafedd56b085fee6ae88a261a
Calvin Franz <cmfranz@uark.edu>
2020-02-29 23:21:05 -0600

    Implement basic Server-side routing on Sign-in page

--- src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
@@ -1,32 +1,58 @@
 package edu.uark.registerapp.controllers;
 
+import java.util.Map;
+import java.util.UUID;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import edu.uark.registerapp.controllers.enums.ViewNames;
+import edu.uark.registerapp.controllers.enums.ViewModelNames;
+import edu.uark.registerapp.models.api.EmployeeSignIn;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
 
 @Controller
-@RequestMapping(value = "/")
+@RequestMapping(value = "/signIn")
 public class SignInRouteController extends BaseRouteController {
-	// TODO: Route for initial page load
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView start(@RequestParam final Map<String, String> queryParameters) {
+		ModelAndView modelAndView;
+		if (true/* employees exist */) {
+			modelAndView = new ModelAndView(ViewNames.SIGN_IN.getViewName());
+		} else {
+			modelAndView = new ModelAndView(ViewNames.EMPLOYEE_DETAIL.getViewName());
+		}
+		return modelAndView;
+	}
 
 	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
 	public ModelAndView performSignIn(
-		// TODO: Define an object that will represent the sign in request and add it as a parameter here
-		HttpServletRequest request
+			final EmployeeSignIn credentials,
+			final HttpServletRequest request
 	) {
 
 		// TODO: Use the credentials provided in the request body
 		//  and the "id" property of the (HttpServletRequest)request.getSession() variable
 		//  to sign in the user
+		ModelAndView modelAndView;
 
-		return new ModelAndView(
-			REDIRECT_PREPEND.concat(
-				ViewNames.MAIN_MENU.getRoute()));
+		if (true/* credentials verified */) {
+			ActiveUserEntity entity = new ActiveUserEntity();
+			entity.setClassification(0/*classification*/);
+			entity.setEmployeeId(UUID.randomUUID()/*EmpId*/);
+			entity.setName("name"/*name*/);
+			entity.setSessionKey("sessKey"/*sessKey*/);
+			modelAndView = new ModelAndView(REDIRECT_PREPEND.concat(ViewNames.MAIN_MENU.getRoute()));
+		} else {
+			modelAndView = new ModelAndView(ViewNames.SIGN_IN.getViewName());
+			modelAndView.addObject(ViewModelNames.ERROR_MESSAGE.getValue(), "Sign in was unsuccessful.");
+		}
+		return modelAndView;
 	}
 }
