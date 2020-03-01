32173b47341acdc87ad59b3e79a6f259bd4ab17d
Calvin Franz <cmfranz@uark.edu>
2020-03-01 00:40:43 -0600

    Implement Sign-in verification functionality.

---
+++ src/main/java/edu/uark/registerapp/commands/employees/EmployeeSignInCommand.java
@@ -0,0 +1,70 @@
+package edu.uark.registerapp.commands.employees;
+
+import java.util.Optional;
+import java.util.Arrays;
+import org.apache.commons.lang3.StringUtils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import edu.uark.registerapp.commands.ResultCommandInterface;
+import edu.uark.registerapp.commands.exceptions.UnprocessableEntityException;
+import edu.uark.registerapp.models.api.EmployeeSignIn;
+import edu.uark.registerapp.models.entities.EmployeeEntity;
+import edu.uark.registerapp.models.repositories.EmployeeRepository;
+
+public class EmployeeSignInCommand implements ResultCommandInterface<EmployeeSignIn>{
+    @Override
+    public EmployeeSignIn execute() {
+        String employeeId = this.apiEmployeeSignIn.getEmployeeID();
+        String password = this.apiEmployeeSignIn.getPassword();
+        this.validateEmployeeId(employeeId);
+        this.validatePassword(password);
+        Optional<EmployeeEntity> employee = employeeRepository.findByEmployeeId(Integer.parseInt(employeeId));
+        this.verifyEmployeeExists(employee);
+        this.verifyCorrectPassword(employee, password.getBytes());
+
+        
+
+        return this.apiEmployeeSignIn;
+    }
+
+    // Helper methods
+    private void validateEmployeeId(String employeeId) {
+        try {
+            Integer.parseInt(employeeId);
+        } catch(NumberFormatException e) {
+            throw new UnprocessableEntityException("EmployeeID");
+        }
+    }
+
+    private void validatePassword(String password) {
+        if (StringUtils.isBlank(password)) {
+            throw new UnprocessableEntityException("Password");
+        }
+    }
+
+    private void verifyEmployeeExists(Optional<EmployeeEntity> employee) {
+        if (!(employee.isPresent())) {
+            throw new UnprocessableEntityException("EmployeeID");
+        }
+    }
+
+    private void verifyCorrectPassword(Optional<EmployeeEntity> employee, byte[] password) {
+        if (Arrays.equals(employee.get().getPassword(), password))
+        {
+            throw new UnprocessableEntityException("Password");
+        }
+    }
+
+    // Properties
+    private EmployeeSignIn apiEmployeeSignIn;
+    public EmployeeSignInCommand setApiEmployeeSignIn(final EmployeeSignIn apiEmployeeSignIn) {
+        this.apiEmployeeSignIn = apiEmployeeSignIn;
+        return this;
+    }
+
+    private String SessionKey;
+
+    @Autowired
+	private EmployeeRepository employeeRepository;
+}
