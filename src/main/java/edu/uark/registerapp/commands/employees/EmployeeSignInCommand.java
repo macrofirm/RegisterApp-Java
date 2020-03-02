17e6dbbbfc97d0556a15a22ac8b307666d81b1fe
Calvin Franz <cmfranz@uark.edu>
2020-03-02 01:00:55 -0600

    Create transaction function for Sign-In

--- src/main/java/edu/uark/registerapp/commands/employees/EmployeeSignInCommand.java
+++ src/main/java/edu/uark/registerapp/commands/employees/EmployeeSignInCommand.java
@@ -2,16 +2,23 @@ package edu.uark.registerapp.commands.employees;
 
 import java.util.Optional;
 import java.util.Arrays;
+import java.util.UUID;
 import org.apache.commons.lang3.StringUtils;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import edu.uark.registerapp.commands.ResultCommandInterface;
 import edu.uark.registerapp.commands.exceptions.UnprocessableEntityException;
 import edu.uark.registerapp.models.api.EmployeeSignIn;
 import edu.uark.registerapp.models.entities.EmployeeEntity;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
 import edu.uark.registerapp.models.repositories.EmployeeRepository;
+import edu.uark.registerapp.models.repositories.ActiveUserRepository;
 
+@Service
 public class EmployeeSignInCommand implements ResultCommandInterface<EmployeeSignIn>{
     @Override
     public EmployeeSignIn execute() {
@@ -19,9 +26,11 @@ public class EmployeeSignInCommand implements ResultCommandInterface<EmployeeSig
         String password = this.apiEmployeeSignIn.getPassword();
         this.validateEmployeeId(employeeId);
         this.validatePassword(password);
-        Optional<EmployeeEntity> employee = employeeRepository.findByEmployeeId(Integer.parseInt(employeeId));
+        Optional<EmployeeEntity> employee = 
+            employeeRepository.findByEmployeeId(Integer.parseInt(employeeId));
         this.verifyEmployeeExists(employee);
         this.verifyCorrectPassword(employee, password.getBytes());
+        this.createActiveUserEntity(employee, employeeId);
 
         return this.apiEmployeeSignIn;
     }
@@ -47,10 +56,34 @@ public class EmployeeSignInCommand implements ResultCommandInterface<EmployeeSig
         }
     }
 
-    private void verifyCorrectPassword(Optional<EmployeeEntity> employee, byte[] password) {
+    private void verifyCorrectPassword(
+        Optional<EmployeeEntity> employee, byte[] password) {
         if (Arrays.equals(employee.get().getPassword(), password))
         {
             throw new UnprocessableEntityException("Password");
+        }
+    }
+
+    @Transactional
+    private ActiveUserEntity createActiveUserEntity(
+        Optional<EmployeeEntity> employee, String employeeId) {
+        final Optional<ActiveUserEntity> queriedActiveUserEntity = 
+            this.activeUserRepository
+                .findByEmployeeId(UUID.fromString(this.apiEmployeeSignIn.getEmployeeID()));
+        if (queriedActiveUserEntity.isPresent()) {
+            queriedActiveUserEntity.get().setSessionKey(SessionKey);
+            activeUserRepository.save(queriedActiveUserEntity.get());
+            return queriedActiveUserEntity.get();
+        } else {
+            ActiveUserEntity newActiveUserEntity = new ActiveUserEntity();
+            newActiveUserEntity.setSessionKey(SessionKey);
+            newActiveUserEntity.setEmployeeId(
+                UUID.fromString(this.apiEmployeeSignIn.getEmployeeID()));
+            newActiveUserEntity.setClassification(employee.get().getClassification());
+            newActiveUserEntity.setName(
+                employee.get().getFirstName() + " " + employee.get().getLastName());
+            activeUserRepository.save(newActiveUserEntity);
+            return newActiveUserEntity;
         }
     }
 
@@ -64,5 +97,6 @@ public class EmployeeSignInCommand implements ResultCommandInterface<EmployeeSig
     private String SessionKey;
 
     @Autowired
-	private EmployeeRepository employeeRepository;
+    private EmployeeRepository employeeRepository;
+    private ActiveUserRepository activeUserRepository;
 }
