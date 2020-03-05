f546c1240ab9d6b5586303afb34763c0edce801d
andrewburroughs <43488350+andrewburroughs@users.noreply.github.com>
2020-03-05 04:18:56 -0600

    Finished Task 9

--- src/main/resources/static/scripts/employeeView.js
+++ src/main/resources/static/scripts/employeeView.js
@@ -1,8 +1,21 @@
+let hideEmployeeSavedAlertTimer = undefined;
 document.addEventListener("DOMContentLoaded", () => {
     getSaveButtonElement().addEventListener("click", saveActionClick);
-
+    //detect if there is an active user or not
+    /*
+    val exists = Packages.edu.uark.registerapp.commands.employees.ActiveEmployeeExistsQuery.execute();
+    val activeUser = Packages.edu.uark.registerapp.commands.activeUsers.ValidateActiveUserCommand.execute();
+    if(user is active){
+        get data of active user
+        activeUser = true;
+    }
+    else{
+        activeUser = false;
+    }
+    */
 });
 
+//save the active user
 function saveActionClick(event){
     if(!validateSave()){
         return;
@@ -11,8 +24,46 @@ function saveActionClick(event){
     const saveActionElement = event.target;
     saveActionElement.disabled = true;
     console.log("Save function called.");
+
+    firstName = getFirstNameElement();
+    lastName = getLastNameElement();
+    password = getPasswordElement();
+    employeeType = getEmployeeTypeElement();
+    displayEmployeeIdRecord(69);
+    setFirstNameElement("Steve");
+    setLastNameElement("Johnson");
     //ToDo: finish code for saving here
     //Use Ajax methods to save the values
+    //POST method if employee is new, ie no active user
+    /*
+    if(activeUser){
+        const saveActionUrl = ("api/Employee/"
+            + ());
+        const saveEmployeeRequest = {
+            firstName: getFirstNameElement(),
+            lastName: getLastNameElement(),
+            password: getPasswordElement(),
+        }
+        ajaxPost(saveActionUrl, saveEmployeeRequest, (callbackResponse) => {
+            saveActionElement.disabled = false;
+            if(isSuccessResponses(callbackResponse)) {
+                displayEmployeeSavedAlertModal();
+            }
+        });
+    }
+    */
+    //PATCH method if employee exists in database, ie is active user
+    /*
+    else{
+        ajaxPatch(saveActionUrl, saveEmployeeRequest, (callbackResponse) => {
+            saveActionElement.disabled = false;
+            if(isSuccessResponses(callbackResponse)){
+                displayEmployeeSavedAlertModal();
+            }
+        });
+    }
+
+    */
 }
 
 function validateSave(){
@@ -20,7 +71,7 @@ function validateSave(){
     lastName = getLastNameElement();
     password = getPasswordElement();
     confirm = getConfirmElement();
-    employeeType = getEmployeeType();
+    employeeType = getEmployeeTypeElement();
     valid = true;
     if(firstName.value == ""){
         displayError("First Name field is blank.");
@@ -42,18 +93,74 @@ function validateSave(){
         displayError("Passwords must match.");
         valid = false;
     }
-    else if(employeeType != "Cashier" && employeeType != "Shift Manager" && employeeType != "General Manager"){
+    else if(employeeType.value != "Cashier" && employeeType.value != "Shift Manager" && employeeType.value != "General Manager"){
         displayError("Employee Type Error.");
         valid = false;
     }
     else{
-        displayError();
+        clearError();
         valid = true;
     }
     return valid;
 }
 
-//getters
+function displayEmployeeSavedAlertModal(){
+    if(hideEmployeeSavedAlertTimer){
+        clearTimeout(hideEmployeeSavedAlertTimer);
+    }
+
+    const savedAlertModalElement = getSavedAlertModalElement();
+    savedAlertModalElement.style.display = "none";
+    savedAlertModalElement.style.display = "block";
+
+    hideEmployeeSavedAlertTimer(hideEmployeeSavedAlertModal, 1200);
+}
+
+function hideEmployeeSavedAlertModal(){
+    if(hideEmployeeSavedAlertTimer){
+        clearTimeout(hideEmployeeSavedAlertTimer);
+    }
+    getSavedAlertModalElement().style.display = "none";
+}
+
+function displayEmployeeIdRecord(employeeId){
+    const employeeIdRowElement = getEmployeeIdRowElement();
+    const employeeIdElement = getEmployeeIdElement();
+
+    employeeIdElement.value = employeeId;
+    if(employeeIdRowElement.classList.contains("hidden")){
+        employeeIdRowElement.classList.remove("hidden");
+    }
+}
+//end save active user functions
+
+//Setters
+function setFirstNameElement(firstName){
+    const firstNameElement = getFirstNameElement();
+    firstNameElement.value = firstName;
+}
+
+function setLastNameElement(lastName){
+    const lastNameElement = getLastNameElement();
+    lastNameElement.value = lastName;
+}
+
+function setPasswordElement(password){
+    const passwordElement = getPasswordElement();
+    passwordElement.value = password;
+}
+
+function setConfirmElement(confirm){
+    const confirmElement = getConfirmElement();
+    confirmElement.value = confirm;
+}
+
+function setEmployeeTypeElement(employeeType){
+    const employeeTypeElement = getEmployeeTypeElement();
+    employeeTypeElement.value = employeeType;
+}
+
+//Getters
 function getFirstNameElement() {
 	return document.getElementById("firstName");
 }
@@ -70,10 +177,26 @@ function getConfirmElement(){
     return document.getElementById("confirm");
 }
 
-function getEmployeeType(){
+function getEmployeeTypeElement(){
     return document.getElementById("employeeType");
+}
+
+function getEmployeeIdRowElement(){
+    return document.getElementById("employeeIdRow");
+}
+
+function getEmployeeIdElement(){
+    return document.getElementById("employeeId");
 }
 
 function getSaveButtonElement(){
     return document.getElementById("saveButton");
+}
+
+function getSignOutActionElement(){
+    return document.getElementById("signOutImage");
+}
+
+function getSavedAlertModalElement(){
+    return document.getElementById("employeeSavedAlertModal");
 }
