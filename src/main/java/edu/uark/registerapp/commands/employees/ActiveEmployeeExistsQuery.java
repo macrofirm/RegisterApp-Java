251785bb6097c373783918996a47adbd271b5950
Calvin Franz <cmfranz@uark.edu>
2020-03-15 18:51:27 -0500

    Make Active Employee Exist Query acces as Service

--- src/main/java/edu/uark/registerapp/commands/employees/ActiveEmployeeExistsQuery.java
+++ src/main/java/edu/uark/registerapp/commands/employees/ActiveEmployeeExistsQuery.java
@@ -1,11 +1,13 @@
 package edu.uark.registerapp.commands.employees;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import edu.uark.registerapp.commands.VoidCommandInterface;
 import edu.uark.registerapp.commands.exceptions.NotFoundException;
 import edu.uark.registerapp.models.repositories.EmployeeRepository;
 
+@Service
 public class ActiveEmployeeExistsQuery implements VoidCommandInterface {
     @Override
     public void execute() {
