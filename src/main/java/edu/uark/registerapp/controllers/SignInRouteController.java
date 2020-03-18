cbfedf98c2ad0770d61833ec8966f9552900deed
Calvin Franz <cmfranz@uark.edu>
2020-03-18 17:04:05 -0500

    Show exact routing error in sign in

--- src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
@@ -51,10 +51,10 @@ public class SignInRouteController extends BaseRouteController {
 		ModelAndView modelAndView;
 		try {
 			this.employeeSignInCommand.setApiEmployeeSignIn(apiEmployeeSignIn).setSessionKey(request.getSession().getId()).execute();
-			modelAndView = new ModelAndView(ViewNames.MAIN_MENU.getViewName());
+			modelAndView = new ModelAndView(REDIRECT_PREPEND.concat(ViewNames.MAIN_MENU.getRoute()));
 		} catch (Exception e) {
 			modelAndView = new ModelAndView(ViewNames.SIGN_IN.getViewName());
-			modelAndView.addObject(ViewModelNames.ERROR_MESSAGE.getValue(), "Sign in was unsuccessful.");
+			modelAndView.addObject(ViewModelNames.ERROR_MESSAGE.getValue(), e.getMessage());
 			modelAndView.addObject(ViewModelNames.EMPLOYEE_ID.getValue(), apiEmployeeSignIn.getEmployeeId());
 		}
 		return modelAndView;
