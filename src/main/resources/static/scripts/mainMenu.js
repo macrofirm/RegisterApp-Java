f30a1213fd51136fcfff3699e7afe99ead528e68
Calvin Franz <cmfranz@uark.edu>
2020-03-18 22:49:08 -0500

    Fix main menu routing

--- src/main/resources/static/scripts/mainMenu.js
+++ src/main/resources/static/scripts/mainMenu.js
@@ -21,12 +21,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
 //events
 function viewProductsClick(event) {
-    location.assign("productListing.html");
+    location.assign("/productListing");
     return;
 }
 
 function createEmployeeClick(event) {
-    location.assign("employeeDetail.html");
+    location.assign("/employeeDetail");
     return;
 }
 
