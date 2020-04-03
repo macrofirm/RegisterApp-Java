aa1fd17bcac8dcce93d9c4b9bbd0aa2030d8299a
andrewburroughs <43488350+andrewburroughs@users.noreply.github.com>
2020-04-02 22:55:28 -0500

    Started Sprint 4

--- src/main/resources/static/scripts/productListing.js
+++ src/main/resources/static/scripts/productListing.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const productListElements = document.getElementById("productsListing").children;
-
+	if(getReturnToCartButtonElement() != null){
+		getReturnToCartButton().addEventListener("click", cartRedirect);
+	}
 	for (let i = 0; i < productListElements.length; i++) {
 		productListElements[i].addEventListener("click", productClick);
 	}
@@ -31,4 +33,40 @@ function productClick(event) {
 	window.location.assign(
 		"/productDetail/"
 		+ listItem.querySelector("input[name='productId'][type='hidden']").value);
+}
+
+function cartRedirect(){
+	alert("Functionality is not yet implemented.  Will redirect to the shopping cart.");
+}
+
+function addToCart (){
+	if(!validateNumber()){
+		return;
+	}
+	alert("Functionality is not yet implemented.  Will add product to cart.");
+}
+
+function validateNumber(){
+	const number = getAddToCartButtonElement();
+	valid = true;
+	if(number == ""){
+		alert("Must enter number of items");
+		valid = false;
+	}
+	else if(number.value() <= 0){
+		alert("Number of items must be greater than 0");
+		valid = false;
+	}
+	else{
+		valid = true;
+	}
+	return valid;
+}
+
+function getAddToCartButtonElement(){
+	return document.getElementById("addToCartButton");
+}
+
+function getReturnToCartButtonElement(){
+	return document.getElementById("returnToCartButton");
 }
