7b056831d67ab992e40ec75f075038175de4f4d4
calvinfranz <60237980+calvinfranz@users.noreply.github.com>
2020-04-09 19:44:56 -0500

    FIx heroku routing error

--- src/main/java/edu/uark/registerapp/controllers/ShoppingCartRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/ShoppingCartRouteController.java
@@ -1,4 +1,8 @@
+package edu.uark.registerapp.controllers;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import edu.uark.registerapp.controllers.enums.ViewNames;
 
