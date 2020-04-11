0b9e96abdcb03cbbd07ed15d40ce962f3329f531
Calvin Franz <cmfranz@uark.edu>
2020-04-11 13:12:10 -0500

    Rout Shopping Cart to TransactionSumamry

--- src/main/java/edu/uark/registerapp/controllers/TransactionSummaryRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/TransactionSummaryRouteController.java
@@ -1,14 +1,20 @@
 package edu.uark.registerapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import edu.uark.registerapp.controllers.enums.ViewNames;
 
 
 //The 'Okay' button is going to route to 'Main Menu'
 @Controller
-@RequestMapping(value = "/")
+@RequestMapping(value = "/transactionSummary")
 public class TransactionSummaryRouteController extends BaseRouteController{
-
-    
-
+    public ModelAndView showTransactionView() {
+        ModelAndView modelAndView =
+            new ModelAndView(ViewNames.TRANSACTION_SUMMARY.getViewName());
+        
+        return modelAndView;
+    }
 }
