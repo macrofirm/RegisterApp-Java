e2fb5dafd11c0bcf894d9d1c1e9da6acc09a554c
Calvin Franz <cmfranz@uark.edu>
2020-04-11 12:17:28 -0500

    Add get request to shopping cart route

--- src/main/java/edu/uark/registerapp/controllers/ShoppingCartRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/ShoppingCartRouteController.java
@@ -3,6 +3,7 @@ package edu.uark.registerapp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.uark.registerapp.controllers.enums.ViewNames;
 
@@ -10,7 +11,7 @@ import edu.uark.registerapp.controllers.enums.ViewNames;
 @Controller
 @RequestMapping(value = "/shoppingCart")
 public class ShoppingCartRouteController {
-    @RequestMapping(value = "/")
+    @RequestMapping(method RequestMethod.GET)
     public ModelAndView showShoppingCart() {
         ModelAndView modelAndView =
             new ModelAndView(ViewNames.SHOPPING_CART.getViewName());
