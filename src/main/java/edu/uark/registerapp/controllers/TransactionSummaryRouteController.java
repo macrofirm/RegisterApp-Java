18e9e7ad3767b797887d2e5f98a107caa22a4dc9
Calvin Franz <cmfranz@uark.edu>
2020-04-11 13:27:22 -0500

    Fix routing for transaction summary

--- src/main/java/edu/uark/registerapp/controllers/TransactionSummaryRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/TransactionSummaryRouteController.java
@@ -3,6 +3,7 @@ package edu.uark.registerapp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.uark.registerapp.controllers.enums.ViewNames;
 
@@ -11,6 +12,7 @@ import edu.uark.registerapp.controllers.enums.ViewNames;
 @Controller
 @RequestMapping(value = "/transactionSummary")
 public class TransactionSummaryRouteController extends BaseRouteController{
+    @RequestMapping(method = RequestMethod.GET)
     public ModelAndView showTransactionView() {
         ModelAndView modelAndView =
             new ModelAndView(ViewNames.TRANSACTION_SUMMARY.getViewName());
