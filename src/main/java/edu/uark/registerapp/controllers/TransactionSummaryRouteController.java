305e6700d659df2c09103d03a47a8683c8ef6787
Nick-Hester <nkhester@email.uark.edu>
2020-04-14 12:55:24 -0500

    fix syntax error

--- src/main/java/edu/uark/registerapp/controllers/TransactionSummaryRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/TransactionSummaryRouteController.java
@@ -1,15 +1,12 @@
 package edu.uark.registerapp.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.uark.registerapp.controllers.enums.ViewNames;
-import edu.uark.registerapp.controllers.enums.ViewModelNames;
-import edu.uark.registerapp.commands.products.ProductsQuery;
-import edu.uark.registerapp.models.api.Product;
+
 
 
 @Controller
@@ -20,21 +17,7 @@ public class TransactionSummaryRouteController extends BaseRouteController{
         ModelAndView modelAndView =
             new ModelAndView(ViewNames.TRANSACTION_SUMMARY.getViewName());
         
-            try {
-                modelAndView.addObject(
-                    ViewModelNames.PRODUCTS.getValue(),
-                    this.productsQuery.execute());
-            } catch (final Exception e) {
-                modelAndView.addObject(
-                    ViewModelNames.ERROR_MESSAGE.getValue(),
-                    e.getMessage());
-                modelAndView.addObject(
-                    ViewModelNames.PRODUCTS.getValue(),
-                    (new Product[0]));
-            }
+        
         return modelAndView;
     }
-
-    @Autowired
-    private ProductsQuery productsQuery;
 }
