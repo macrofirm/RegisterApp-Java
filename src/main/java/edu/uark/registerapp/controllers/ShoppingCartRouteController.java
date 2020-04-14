d3ceaa54d88fa3843f0fdb4734af602ab8a60871
calvinfranz <60237980+calvinfranz@users.noreply.github.com>
2020-04-14 10:48:38 -0500

    Route product items to shopping cart

--- src/main/java/edu/uark/registerapp/controllers/ShoppingCartRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/ShoppingCartRouteController.java
@@ -1,11 +1,15 @@
 package edu.uark.registerapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.uark.registerapp.controllers.enums.ViewNames;
+import edu.uark.registerapp.controllers.enums.ViewModelNames;
+import edu.uark.registerapp.commands.products.ProductsQuery;
+import edu.uark.registerapp.models.api.Product;
 
 
 @Controller
@@ -16,6 +20,23 @@ public class ShoppingCartRouteController {
         ModelAndView modelAndView =
             new ModelAndView(ViewNames.SHOPPING_CART.getViewName());
 
+            try {
+                modelAndView.addObject(
+                    ViewModelNames.PRODUCTS.getValue(),
+                    this.productsQuery.execute());
+            } catch (final Exception e) {
+                modelAndView.addObject(
+                    ViewModelNames.ERROR_MESSAGE.getValue(),
+                    e.getMessage());
+                modelAndView.addObject(
+                    ViewModelNames.PRODUCTS.getValue(),
+                    (new Product[0]));
+            }
+
         return modelAndView;
     }
+
+    // Properties
+	@Autowired
+	private ProductsQuery productsQuery;
 }
