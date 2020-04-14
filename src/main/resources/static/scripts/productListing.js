b87a89e5bcbc30bbca5cae397e8341e296c4bf92
Jett McCullough <jett.mccullough99@gmail.com>
2020-04-14 13:55:42 -0500

    implementing out of stock display on productListing

--- src/main/resources/static/scripts/productListing.js
+++ src/main/resources/static/scripts/productListing.js
@@ -6,6 +6,11 @@ document.addEventListener("DOMContentLoaded", () => {
 	for (let i = 0; i < productListElements.length; i++) {
 		productListElements[i].addEventListener("click", productClick);
 	}
+	for (let i = 0; i < productListElements.length; i++) {
+		if(document.getElementsByClassName("productCountDisplay")[i] == "0") {
+			productListElements[i].productCountDisplay = "Out of Stock";
+		}
+	}
 });
 
 function findClickedListItemElement(clickedTarget) {
@@ -64,6 +69,7 @@ function validateNumber(){
 	return valid;
 }
 
+// getters
 function getAddToCartButtonElement(){
 	return document.getElementById("addToCartButton");
 }
