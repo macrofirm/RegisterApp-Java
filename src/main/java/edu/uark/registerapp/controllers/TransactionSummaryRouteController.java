d9dad65457cc5dc65f1c562be067ad03fafb910d
calvinfranz <60237980+calvinfranz@users.noreply.github.com>
2020-04-14 11:53:15 -0500

    Remove unnecessary auto repos

--- src/main/java/edu/uark/registerapp/controllers/TransactionSummaryRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/TransactionSummaryRouteController.java
@@ -1,13 +1,11 @@
 package edu.uark.registerapp.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.uark.registerapp.controllers.enums.ViewNames;
-import edu.uark.registerapp.commands.products.ProductsQuery;
 
 
 //The 'Okay' button is going to route to 'Main Menu'
@@ -21,7 +19,4 @@ public class TransactionSummaryRouteController extends BaseRouteController{
         
         return modelAndView;
     }
-
-    @Autowired
-    private ProductsQuery productsQuery;
 }
