4105dd421027e50ebdc89195a38a3789512ed45e
Nick-Hester <nkhester@email.uark.edu>
2020-04-14 11:23:48 -0500

    adds to 'TransactionSummaryRouteController.java'

--- src/main/java/edu/uark/registerapp/controllers/TransactionSummaryRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/TransactionSummaryRouteController.java
@@ -1,11 +1,15 @@
 package edu.uark.registerapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.uark.registerapp.controllers.enums.ViewNames;
+import edu.uark.registerapp.controllers.enums.ViewModelNames;
+import edu.uark.registerapp.commands.products.ProductsQuery;
+import edu.uark.registerapp.models.api.Product;
 
 
 //The 'Okay' button is going to route to 'Main Menu'
@@ -17,6 +21,21 @@ public class TransactionSummaryRouteController extends BaseRouteController{
         ModelAndView modelAndView =
             new ModelAndView(ViewNames.TRANSACTION_SUMMARY.getViewName());
         
+            try {
+                modelAndView.addObject(
+                    ViewModelNames.PRODUCTS.getValue(),
+                    this.productsQuery.execute());
+            } catch (final Excepeiton e) {
+                modelAndView.addObject(
+                    viewModelNames.ERROR_MESSAGE.getValue(),
+                    e.getMessage());
+                modelAndView.addObject(
+                    ViewModelNames.PRODUCTS.getValue(),
+                    new Product[0]));
+            }
         return modelAndView;
     }
+
+    @Autowired
+    private ProductsQuery productsQuery;
 }
