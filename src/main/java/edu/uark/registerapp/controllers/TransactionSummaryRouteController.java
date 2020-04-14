80b85b94fae9065770cd51fc3e5fe2e06c702ffb
calvinfranz <60237980+calvinfranz@users.noreply.github.com>
2020-04-14 11:52:43 -0500

    Remove product values

--- src/main/java/edu/uark/registerapp/controllers/TransactionSummaryRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/TransactionSummaryRouteController.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.uark.registerapp.controllers.enums.ViewNames;
-import edu.uark.registerapp.controllers.enums.ViewModelNames;
 import edu.uark.registerapp.commands.products.ProductsQuery;
-import edu.uark.registerapp.models.api.Product;
 
 
 //The 'Okay' button is going to route to 'Main Menu'
@@ -21,18 +19,6 @@ public class TransactionSummaryRouteController extends BaseRouteController{
         ModelAndView modelAndView =
             new ModelAndView(ViewNames.TRANSACTION_SUMMARY.getViewName());
         
-            try {
-                modelAndView.addObject(
-                    ViewModelNames.PRODUCTS.getValue(),
-                    this.productsQuery.execute());
-            } catch (final Excepeiton e) {
-                modelAndView.addObject(
-                    viewModelNames.ERROR_MESSAGE.getValue(),
-                    e.getMessage());
-                modelAndView.addObject(
-                    ViewModelNames.PRODUCTS.getValue(),
-                    (new Product[0]));
-            }
         return modelAndView;
     }
 
