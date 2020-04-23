8049a98839bad7a42c013bcd7011238cfd4e23fd
andrewburroughs <43488350+andrewburroughs@users.noreply.github.com>
2020-04-23 12:16:30 -0500

    Working on Price

--- src/main/java/edu/uark/registerapp/models/api/Product.java
+++ src/main/java/edu/uark/registerapp/models/api/Product.java
@@ -75,7 +75,7 @@ public class Product extends ApiResponse {
 		this.count = -1;
 		this.id = new UUID(0, 0);
 		this.lookupCode = StringUtils.EMPTY;
-		this.price = 0;
+		this.price = 0.00;
 
 		this.setCreatedOn(LocalDateTime.now());
 	}
