a7a4fac550801737f94d102f98486d35a5b8c447
Nick-Hester <nkhester@email.uark.edu>
2020-04-29 17:27:32 -0500

    implement total price functionality

--- src/main/java/edu/uark/registerapp/controllers/TransactionSummaryRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/TransactionSummaryRouteController.java
@@ -1,6 +1,7 @@
 package edu.uark.registerapp.controllers;
 
 import java.util.UUID;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import edu.uark.registerapp.controllers.enums.ViewNames;
 import edu.uark.registerapp.controllers.enums.ViewModelNames;
 import edu.uark.registerapp.commands.transactions.TransactionEntriesQuery;
+import edu.uark.registerapp.models.api.TransactionEntry;
+import edu.uark.registerapp.models.entities.TransactionEntryEntity;
+import edu.uark.registerapp.models.repositories.TransactionEntryRepository;
 
 
 @Controller
@@ -23,12 +27,20 @@ public class TransactionSummaryRouteController extends BaseRouteController{
             new ModelAndView(ViewNames.TRANSACTION_SUMMARY.getViewName());
         
             try {
+                List<TransactionEntry> transactionEntries = this.transactionEntriesQuery.setTransactionId(transactionId).execute();
+                double totalPrice = 0;
+                for(int i = 0; i < transactionEntries.size(); i++)
+                {
+                    totalPrice += transactionEntries.get(i).getPrice() * transactionEntries.get(i).getQuantity();
+                }
                 modelAndView.addObject(
                     ViewModelNames.TRANSACTION_ENTRIES.getValue(),
-                    this.transactionEntriesQuery.setTransactionId(transactionId).execute());
+                    transactionEntries);
                 modelAndView.addObject(
                     ViewModelNames.TRANSACTION_ID.getValue(),
                     transactionId);
+                modelAndView.addObject(
+                    "TotalPrice", totalPrice);
             } catch (final Exception e) {
                 modelAndView.addObject(
                     ViewModelNames.ERROR_MESSAGE.getValue(),
