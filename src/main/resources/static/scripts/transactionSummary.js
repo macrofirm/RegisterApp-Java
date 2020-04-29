2755a57121159d713633a7a0c9189edd76db87bf
Nick-Hester <nkhester@email.uark.edu>
2020-04-29 16:48:35 -0500

    removed quantity counter in transactionSummary

--- src/main/resources/static/scripts/transactionSummary.js
+++ src/main/resources/static/scripts/transactionSummary.js
@@ -3,29 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	if (getOkayButtonElement() != null) {
 		getOkayButtonElement().addEventListener("click", okayButtonClickHandler);
 	}
-});
-
-var quantity;
-		
-function incrementValue(){
-
-    quantity = parseInt(document.getElementById('number').value, 10);
-   	quantity = isNaN(quantity) ? 0 : quantity;
-	quantity++;
-	document.getElementById('number').value = quantity;
-			
-}
-
-function decrementValue(){
-
-	quantity = parseInt(document.getElementById('number').value, 10);
-	quantity = isNaN(quantity) ? 0 : quantity;
-	if(quantity > 0)
-		quantity--;
-	else
-		quantity = 0;
-	document.getElementById('number').value = quantity;
-} 
+}); 
 
 function okayButtonClickHandler() {
 
