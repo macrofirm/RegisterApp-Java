c2705f8f92796daea063730da819e69a02c0c323
Nick-Hester <nkhester@email.uark.edu>
2020-04-29 17:59:03 -0500

    Fix currency issues

--- src/main/resources/static/scripts/transactionSummary.js
+++ src/main/resources/static/scripts/transactionSummary.js
@@ -3,7 +3,24 @@ document.addEventListener("DOMContentLoaded", () => {
 	if (getOkayButtonElement() != null) {
 		getOkayButtonElement().addEventListener("click", okayButtonClickHandler);
 	}
-}); 
+});
+
+var list = document.getElementsByClassName("productPriceDisplay");
+	for(let i = 0; i<list.length; i++){
+		var x = list[i].innerHTML;
+		var len = x.length;
+		var pos = x.indexOf(".");
+		if(pos == -1){
+			x += ".";
+			pos = x.indexOf(".");
+		}
+		var diff = len - pos;
+		var newText = "$" + x;
+		for(let j = diff; j<3; j++){
+			newText += "0"
+		}
+		list[i].innerHTML = newText;
+	}
 
 function okayButtonClickHandler() {
 
